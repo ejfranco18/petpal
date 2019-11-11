1a32b90e2dc6b42b00d0b9b31a4f09f9895719f7
Javier Franco <javier.franco18@gmail.com>
2019-11-11 16:35:19 +0100

    fixes on migraitons

--- db/migrate/20191111152002_add_attr_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddAttrToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :pet_name, :string
-    add_column :users, :pet_type, :string
-    add_column :users, :role, :string
-  end
-end
