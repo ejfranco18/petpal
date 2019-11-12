4f7ffa8f9a36ed948b35803217199526e73fb532
Javier Franco <javier.franco18@gmail.com>
2019-11-12 17:03:54 +0100

    Updates forms and creation of petplaces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,8 +6,10 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @petplaces = Petplace.where("user_id = #{current_user.id}")
-
-
+    if current_user
+      @petplaces = Petplace.where("user_id = #{current_user.id}")
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
