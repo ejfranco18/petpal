42d9bd499a8ec828bcdcfadb9757045f53841989
Niklas Wagener <n-wagener@web.de>
2019-11-12 17:04:09 +0100

    further developed appointments controlle and created routes for  appointments (only new & create so far)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :petplaces
+  resources :petplaces do
+    resources :appointments, only: [:new, :create]
+  end
+
+  resources :appointments, only: [:show]
+  # add other routes  for  appointments [:index, show,]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
