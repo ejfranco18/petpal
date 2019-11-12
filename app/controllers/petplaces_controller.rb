08905381fc70672e01fb6bfa797290d928c6feb9
Herve Lao <laoherve@outlook.com>
2019-11-12 11:35:23 +0100

    pundit

--- app/controllers/petplaces_controller.rb
+++ app/controllers/petplaces_controller.rb
@@ -1,9 +1,11 @@
 class PetplacesController < ApplicationController
   def index
-    @petplaces = Petplace.all
+    # @petplaces = Petplace.all
+    @petplaces = policy_scope(Petplace)
   end
 
   def show
     @petplace = Petplace.find(params[:id])
+    authorize @petplace
   end
 end
