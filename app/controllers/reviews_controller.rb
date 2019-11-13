a856d93ff7e11a2067d82f7a66b3af262420beb7
Herve Lao <laoherve@outlook.com>
2019-11-13 12:08:03 +0100

    reviews and review/_new.html.erb done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+  def create
+    @appointment = Appointment.find(params[:appointment_id])
+    @review = Review.new(review_params)
+    @review.appointment = @appointment
+    @review.save
+    redirect_to petplace_path(@petplace)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :description, :images)
+  end
 end
