c206c6a1cdaa955fa0a0c5d4b7aafcb3459bfa53
Niklas Wagener <n-wagener@web.de>
2019-11-14 15:12:30 +0100

    changed style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,9 @@ class PagesController < ApplicationController
 
   def dashboard
     if current_user
-      @petplaces = Petplace.where("user_id = #{current_user.id}")
+      @petplaces = Petplace.where(user: current_user)
+      @appointments_booked = Appointment.where(user: current_user)
+      @all_my_appointments = @petplaces.where(&:appointments)
     else
       redirect_to new_user_session_path
     end
