f288bff41c6b6787cbb7367f877dbd76747d2e13
Maja Polanc <mayainstudio@gmail.com>
2019-11-15 13:59:14 +0100

    booking forms completed

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,5 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/dark.css"  // Note this is important!
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {minDate: new Date()}
+    )
